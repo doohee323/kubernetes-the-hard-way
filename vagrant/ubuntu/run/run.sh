121ad213cb91ae96c25377e1cd8b4aed3e948521
Dewey Hong <doohee323@gmail.com>
2020-07-20 23:52:16 -0700

    make run folder for run.sh scripts

--- vagrant/ubuntu/run/run.sh
+++ vagrant/ubuntu/run/run.sh
@@ -6,7 +6,7 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " 01.setting ssh among nodes "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 ssh -i ../.vagrant/machines/master-1/virtualbox/private_key \
-    vagrant@192.168.5.11 /bin/bash /vagrant/ubuntu/ubuntu/make_sshkey.sh
+    vagrant@192.168.5.11 /bin/bash /vagrant/run/01.make_sshkey.sh
 
 echo "appending master's .ssh/id_rsa.pub to other nodes' authorized_keys!"
 for e in master_2 worker_1 worker_2 loadbalancer; do
@@ -24,7 +24,7 @@ for e in master_2 worker_1 worker_2 loadbalancer; do
         instance='loadbalancer'
     fi
     ssh -i ../.vagrant/machines/${instance}/virtualbox/private_key vagrant@${ip} \
-        /bin/bash /vagrant/ubuntu/ubuntu/append_sshkey.sh
+        /bin/bash /vagrant/ubuntu/run/append_sshkey.sh
 done
 
 rm -Rf authorized_keys
@@ -33,36 +33,36 @@ echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " 02.tls_certificates.sh "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 ssh -i ../.vagrant/machines/master-1/virtualbox/private_key vagrant@192.168.5.11 \
-        /bin/bash /vagrant/ubuntu/vagrant/02.tls_certificates.sh
+        /bin/bash /vagrant/ubuntu/run/02.tls_certificates.sh
 
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " 03.k8s_auth.sh "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 ssh -i ../.vagrant/machines/master-1/virtualbox/private_key vagrant@192.168.5.11 \
-        /bin/bash /vagrant/ubuntu/vagrant/03.k8s_auth.sh
+        /bin/bash /vagrant/ubuntu/run/03.k8s_auth.sh
 
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " 04.encryt_key.sh "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 ssh -i ../.vagrant/machines/master-1/virtualbox/private_key vagrant@192.168.5.11 \
-        /bin/bash /vagrant/ubuntu/vagrant/04.encryt_key.sh
+        /bin/bash /vagrant/ubuntu/run/04.encryt_key.sh
 
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " 05.etcd_server.sh "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 ssh -i ../.vagrant/machines/master-1/virtualbox/private_key vagrant@192.168.5.11 \
-        /bin/bash /vagrant/ubuntu/vagrant/05.etcd_server.sh
+        /bin/bash /vagrant/ubuntu/run/05.etcd_server.sh
 sleep 3
 ssh -i ../.vagrant/machines/master-2/virtualbox/private_key vagrant@192.168.5.12 \
-        /bin/bash /vagrant/ubuntu/vagrant/05.etcd_server.sh
+        /bin/bash /vagrant/ubuntu/run/05.etcd_server.sh
 
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo " 06-1.control_plane.sh / 06-2.loadbalancer.sh "
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 ssh -i ../.vagrant/machines/master-1/virtualbox/private_key vagrant@192.168.5.11 \
-        /bin/bash /vagrant/ubuntu/vagrant/06-1.control_plane.sh
+        /bin/bash /vagrant/ubuntu/run/06-1.control_plane.sh
 ssh -i ../.vagrant/machines/master-2/virtualbox/private_key vagrant@192.168.5.12 \
-        /bin/bash /vagrant/ubuntu/vagrant/06-1.control_plane.sh
+        /bin/bash /vagrant/ubuntu/run/06-1.control_plane.sh
 sleep 3
 ssh -i ../.vagrant/machines/loadbalancer/virtualbox/private_key vagrant@192.168.5.30 \
-        /bin/bash /vagrant/ubuntu/vagrant/06-2.loadbalancer.sh
+        /bin/bash /vagrant/ubuntu/run/06-2.loadbalancer.sh
